20dac8057d2dc96ba51b7eebb0b0c0e4ed25731d
cholzweiss <holzweiss@gmail.com>
2018-10-23 23:28:25 +0200

    Euler1

--- Euler1.js
+++ Euler1.js
@@ -1,3 +1,6 @@
 //Euler 1
-let a=Array.from(new Array(1000),(x,i)=>i)
-console.log(a.map(x=>x%3==0||x%5==0?x:0).reduce((xs,x)=>xs+x))
+let a=Array
+	.from(new Array(1000),(x,i)=>i)
+	.map(x=>x%3==0||x%5==0?x:0)
+	.reduce((xs,x)=>xs+x)
+console.log(a)
