04625d813e67c1a0c79f1b767930442ed0a1db05
cholzweiss <holzweiss@gmail.com>
2025-01-06 15:20:02 +0100

    Update Euler10.js

--- Euler10.js
+++ Euler10.js
@@ -4,3 +4,19 @@ let a = Array.from(Array(2000000), (_, n)=>n).slice(2)
 	.filter(x=>!x[1]).map(x=>x[0])
 	.reduce((acc, curr)=>acc+curr)
 console.log(a, Date.now()-start)
+
+/* ------------ Much faster: ------------------- */
+
+let start = Date.now()
+let a=Array(2000000).fill(0).map((_,i)=>{
+	if(i<2) return false;
+	let x = 2;
+	while(x<=Math.sqrt(i)){
+		if(i % x == 0) return [i, false]
+		x++
+	}
+	return [i,true]
+})
+.filter(x=>x[1]).map(x=>x[0])
+.reduce((acc, curr)=>acc+curr)
+console.log(a, Date.now()-start)
