25b3e68ebfda53bdf70e4be33cde643740061fd1
cholzweiss <holzweiss@gmail.com>
2025-02-24 14:36:47 +0100

    Update Euler85.hs

--- Euler85.hs
+++ Euler85.hs
@@ -6,6 +6,4 @@ g o = [(f m n, m, n) | m<-[1..o], n<-[1..o]]
 
 main :: IO ()
 main = do
-  print (f 4 1)
-  let gm = g 1000
-  print (filter(\(z,_,_) -> z < 2000000 +10 && z > 2000000 -10) gm)
+  print (filter(\(z,_,_) -> z < 2000000 +10 && z > 2000000 -10) (g 1000))
